75b6057a311a8054c8ccd63b3c842d511504ceb0
Jacob <92279567+Oia20@users.noreply.github.com>
2024-11-29 17:39:25 -0600

    Update vite.config.js

--- portfolio-about/vite.config.js
+++ portfolio-about/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Add for deploy. remove for running
+  // Add for deploy. remove for dev...
   base: "/PortfolioAbout",
 })
