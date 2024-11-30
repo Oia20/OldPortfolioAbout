0735fc07d5fecd07d3e355519f03a04b5f9150d7
Jacob <92279567+Oia20@users.noreply.github.com>
2024-11-29 18:01:42 -0600

    Fixed deployment errors.

--- portfolio-about/vite.config.js
+++ portfolio-about/vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Add for deploy. remove for dev...
-  base: "/PortfolioAbout",
+  base: "/OldPortfolioAbout",
 })
